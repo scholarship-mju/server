fe4c087197a45dd037bec4c1ea6f35698b5ddeed
taek <wontaek88@gmail.com>
2025-02-02 16:51:47 +0900

    Fix: 전테 장학금 조회 & 받은 장학금 조회 API 필요없는 필드 dto에서 삭제

--- src/main/java/mju/scholarship/scholoarship/dto/AllScholarshipResponse.java
+++ src/main/java/mju/scholarship/scholoarship/dto/AllScholarshipResponse.java
@@ -12,42 +12,16 @@ public class AllScholarshipResponse {
 
     private Long id;
     private String name; // 장학금 이름
-    private String detailEligibility; //신청 자격
-    private String price; //
-    private String university; // 대학교
-    private Integer minAge; // 최소 나이
-    private Integer maxAge; // 최대 나이
-    private String gender; // 성별
-    private String startDate; // 신청 시작 날짜
-    private String endDate; // 신청 종료 날짜
-    private String submission; // 제출 파일
-    private String province; // 도/광역시
-    private String city; // 시
-    private String department; // 학과
-    private Double grade; // 학점
-    private Integer incomeQuantile; // 소득분위
-    private Boolean isInterested;
-    private ScholarshipProgressStatus progressStatus;
+    private String price; // 장학금 가격
+    private Boolean isInterested; // 찜한 장학금인지
+    private ScholarshipProgressStatus progressStatus; // 장학금 진행 상태
 
     @Builder
-    public AllScholarshipResponse(Long id, String name, String detailEligibility, String price, String university, Integer minAge, Integer maxAge, String gender, String startDate, String endDate, String submission, String province, String city, String department, Double grade, Integer incomeQuantile, ScholarshipProgressStatus progressStatus, Boolean isInterested) {
+    public AllScholarshipResponse(Long id, String name, String price, Boolean isInterested, ScholarshipProgressStatus progressStatus) {
         this.id = id;
         this.name = name;
-        this.detailEligibility = detailEligibility;
         this.price = price;
-        this.university = university;
-        this.minAge = minAge;
-        this.maxAge = maxAge;
-        this.gender = gender;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.submission = submission;
-        this.province = province;
-        this.city = city;
-        this.department = department;
-        this.grade = grade;
-        this.incomeQuantile = incomeQuantile;
-        this.progressStatus = progressStatus;
         this.isInterested = isInterested;
+        this.progressStatus = progressStatus;
     }
 }
--- src/main/java/mju/scholarship/scholoarship/dto/GotScholarshipResponse.java
+++ src/main/java/mju/scholarship/scholoarship/dto/GotScholarshipResponse.java
@@ -3,6 +3,7 @@ package mju.scholarship.scholoarship.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import mju.scholarship.member.entity.ScholarshipStatus;
 import mju.scholarship.scholoarship.ScholarshipProgressStatus;
 
 @Getter
@@ -11,40 +12,14 @@ public class GotScholarshipResponse {
 
     private Long id;
     private String name; // 장학금 이름
-    private String detailEligibility; //신청 자격
-    private String price; //
-    private String university; // 대학교
-    private Integer minAge; // 최소 나이
-    private Integer maxAge; // 최대 나이
-    private String gender; // 성별
-    private String startDate; // 신청 시작 날짜
-    private String endDate; // 신청 종료 날짜
-    private String submission; // 제출 파일
-    private String province; // 도/광역시
-    private String city; // 시
-    private String department; // 학과
-    private Double grade; // 학점
-    private Integer incomeQuantile; // 소득분위
-    private ScholarshipProgressStatus progressStatus;
+    private String price; // 장학금 가격
+    private ScholarshipStatus status; // 받은 장학금 인증 상태
 
     @Builder
-    public GotScholarshipResponse(Long id, String name, String detailEligibility, String price, String university, Integer minAge, Integer maxAge, String gender, String startDate, String endDate, String submission, String province, String city, String department, Double grade, Integer incomeQuantile, ScholarshipProgressStatus progressStatus) {
+    public GotScholarshipResponse(Long id, String name, String price, ScholarshipStatus status) {
         this.id = id;
         this.name = name;
-        this.detailEligibility = detailEligibility;
         this.price = price;
-        this.university = university;
-        this.minAge = minAge;
-        this.maxAge = maxAge;
-        this.gender = gender;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.submission = submission;
-        this.province = province;
-        this.city = city;
-        this.department = department;
-        this.grade = grade;
-        this.incomeQuantile = incomeQuantile;
-        this.progressStatus = progressStatus;
+        this.status = status;
     }
 }
