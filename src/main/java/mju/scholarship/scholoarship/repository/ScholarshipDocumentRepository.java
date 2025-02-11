00e15413aaad3bf924542cdb6976ded85b6697e1
taek <wontaek88@gmail.com>
2025-02-11 18:49:19 +0900

    Delete: 엘라스틱서치 부분 임시 삭제

--- src/main/java/mju/scholarship/scholoarship/repository/ScholarshipDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package mju.scholarship.scholoarship.repository;
-
-import mju.scholarship.scholoarship.ScholarshipDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ScholarshipDocumentRepository extends ElasticsearchRepository<ScholarshipDocument, Long> {
-}
