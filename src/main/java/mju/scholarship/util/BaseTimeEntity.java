854b2b60cd3bbcfe5db8cf6d950f3e5ed0a47d94
taek <wontaek88@gmail.com>
2025-02-12 20:47:06 +0900

    Feat: 생성, 수정 시간 기록을 위한 엔티티

---
+++ src/main/java/mju/scholarship/util/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package mju.scholarship.util;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass // 추후에 BaseTimeEntity를 상속한 엔티티들을 아래 필드들을 컬럼으로 인식
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
