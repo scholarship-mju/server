1a1a4fb10ad49d59c3b9c500df92b51b451f6d7d
taek <wontaek88@gmail.com>
2025-02-12 20:47:55 +0900

    Feat: BaseTimeEntity 시간 서울 기준으로 설정

--- src/main/java/mju/scholarship/ScholarshipApplication.java
+++ src/main/java/mju/scholarship/ScholarshipApplication.java
@@ -1,15 +1,25 @@
 package mju.scholarship;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaAuditing
 public class ScholarshipApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScholarshipApplication.class, args);
+	}
+
+	@PostConstruct
+	public void setTime() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
