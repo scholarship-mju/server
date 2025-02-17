9f7b34fd1d6a7e0be8cf7b9cb6477179a20265d2
taek <wontaek88@gmail.com>
2025-02-17 13:29:07 +0900

    Fix: 장학금 전체 조회 API dto에  viewCount 추가

--- src/main/java/mju/scholarship/scholoarship/dto/GotScholarshipResponse.java
+++ src/main/java/mju/scholarship/scholoarship/dto/GotScholarshipResponse.java
@@ -14,12 +14,14 @@ public class GotScholarshipResponse {
     private String name; // 장학금 이름
     private String price; // 장학금 가격
     private ScholarshipStatus status; // 받은 장학금 인증 상태
+    private int viewCount;
 
     @Builder
-    public GotScholarshipResponse(Long id, String name, String price, ScholarshipStatus status) {
+    public GotScholarshipResponse(Long id, String name, String price, ScholarshipStatus status, int viewCount) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.status = status;
+        this.viewCount = viewCount;
     }
 }
