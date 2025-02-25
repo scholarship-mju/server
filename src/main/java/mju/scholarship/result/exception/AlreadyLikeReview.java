c0b2585689d65acce84f856b1013b2679669638d
taek <wontaek88@gmail.com>
2025-02-25 23:08:02 +0900

    Update: 리뷰 좋아요 API 리뷰 좋아요 중복 exception

---
+++ src/main/java/mju/scholarship/result/exception/AlreadyLikeReview.java
@@ -0,0 +1,9 @@
+package mju.scholarship.result.exception;
+
+import mju.scholarship.result.code.ErrorCode;
+
+public class AlreadyLikeReview extends BusinessException {
+
+    public AlreadyLikeReview() {
+        super(ErrorCode.AlreadyLikeReview);
+}
