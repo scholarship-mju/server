54b54d8a34db6866c8a9fedafd4c795fa61222f1
taek <wontaek88@gmail.com>
2025-03-04 14:19:05 +0900

    Refactor: 에러 코드, 결과 코드 잘못된 상태코드 변경 + 주석으로 보기 쉽게 정리

--- src/main/java/mju/scholarship/result/code/ErrorCode.java
+++ src/main/java/mju/scholarship/result/code/ErrorCode.java
@@ -7,29 +7,35 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    MemberNotFoundException(500, "M501", "유저가 존재하지 않습니다(토큰오류)"),
-    IllegalRegistrationIdException(500, "A501", "Oauth 로그인 오류"),
-    TokenException(500, "T501", "정상적인 토큰이 아닙니다"),
-    TokenInvalidException(500, "T502", "검증된 토큰이 아닙니다"),
-    TokenExpiredException(500, "T503", "토큰이 만료되었씁니다"),
-    JwtSignatureInvalid(500, "T504", "서명이 검증되지 않음"),
-    RefreshTokenNotFoundException(500, "T505", "리프레시 토큰을 찾을 수 없음"),
-    TokenNotFoundException(500, "T506", "토큰을 찾을 수 없습니다"),
+    // 유저
+    MemberNotFoundException(404, "M401", "유저가 존재하지 않습니다(토큰오류)"),
+    IllegalRegistrationIdException(401, "A501", "Oauth 로그인 오류"),
 
-    ScholarshipNotFoundException(500, "S507", "장학금을 찾을 수 없습니다"),
-    AlreadyGotScholarshipException(500, "S508", "이미 받은 장학금에 등록되었습니다"),
-    AlreadyInterestedScholarshipException(500, "S509", "이미 찜하였습니다"),
-    GotScholarshipNotFoundException(500, "S510", "이미 받은 장학금을 찾을 수 없습니다"),
-    InterestedScholarshipNotFoundException(500, "S511", "찜한 장학금을 찾을 수 없습니다"),
+    // 토큰
+    TokenException(401, "T401", "정상적인 토큰이 아닙니다"),
+    TokenInvalidException(401, "T402", "검증된 토큰이 아닙니다"),
+    TokenExpiredException(401, "T403", "토큰이 만료되었씁니다"),
+    JwtSignatureInvalid(401, "T404", "서명이 검증되지 않음"),
+    RefreshTokenNotFoundException(401, "T405", "리프레시 토큰을 찾을 수 없음"),
+    TokenNotFoundException(401, "T406", "토큰을 찾을 수 없습니다"),
 
+    // 장학금
+    ScholarshipNotFoundException(404, "S401", "장학금을 찾을 수 없습니다"),
+    AlreadyGotScholarshipException(400, "S402", "이미 받은 장학금에 등록되었습니다"),
+    AlreadyInterestedScholarshipException(400, "S403", "이미 찜하였습니다"),
+    GotScholarshipNotFoundException(404, "S404", "이미 받은 장학금을 찾을 수 없습니다"),
+    InterestedScholarshipNotFoundException(404, "S405", "찜한 장학금을 찾을 수 없습니다"),
+    CsvParsingException(500, "S501", "장학금 파싱 실패"),
 
+    // 파일
     FileConvertException(500, "F501", "파일 변환 실패"),
     FileUploadException(500, "F502", "파일 업로드 실패"),
 
+    // 리뷰
+    ReviewNotFoundException(404, "R401", "리뷰를 찾을 수 없습니다"),
+    AlreadyLikeReview(400, "R402", "이미 좋아요를 누른 리뷰입니다"),
+    NotFoundLikeReview(400, "R403", "리뷰에 좋아요를 누르지 않았습니다"),
 
-    ReviewNotFoundException(500, "R001", "리뷰를 찾을 수 없습니다"),
-    AlreadyLikeReview(500, "R002", "이미 좋아요를 누른 리뷰입니다"),
-    NotFoundLikeReview(500, "R003", "리뷰에 좋아요를 누르지 않았습니다"),
     ;
 
 
--- src/main/java/mju/scholarship/result/code/ResultCode.java
+++ src/main/java/mju/scholarship/result/code/ResultCode.java
@@ -7,20 +7,27 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResultCode {
 
+    // 유저
     CreateInfoSuccess(200, "M202", "회원 정보 등록 성공"),
     ProfileUpdateSuccess(200, "M203", "프로필 변경 성공"),
-    CreateScholarshipSuccess(200, "S201", "장학금 생성 성공"),
+
+    //장학금
+    UploadScholarshipSuccess(200, "S201", "장학금 등록 성공"),
     InterestScholarshipSuccess(200, "S205", "장학금 찜하기 성공"),
     DeleteInterestScholarshipSuccess(200, "S206", "찜한 장학금 제거 성공"),
     AddGotScholarshipSuccess(200, "S206", "받은 장학금 등록 성공"),
     ValidGotScholarshipSuccess(200, "S207", "장학금 검증 사지 등록 성공"),
     DeleteGotScholarshipSuccess(200, "S207", "받은 장학금 제거 성공"),
     ValidAddGotScholarshipSuccess(200, "S208", "인증된 받은 장학금 추가"),
+
+    //
     FirstLoginSuccess(200, "L201", "첫번째 로그인 성공"),
+
+    //리뷰
     CreateReviewSuccess(200, "R001", "리뷰 생성 성공"),
     LikeReviewSuccess(200, "R002", "리뷰 좋아요 성공"),
     CancelLikeReview(200, "R003", "리뷰 좋아요 취소 성공"),
-    DeleteReviewSuccess(200, "R003", "리뷰 삭제 성공")
+    DeleteReviewSuccess(200, "R003", "리뷰 삭제 성공"),
     ;
 
     private final int status;
