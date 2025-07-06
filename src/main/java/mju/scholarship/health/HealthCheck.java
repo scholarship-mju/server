23081d5e5cc10c51857eea2fffd56bedf5d0bd99
taek <wontaek88@gmail.com>
2025-07-06 18:55:47 +0900

    Fix: 헬스 체크 API 빈 추가

--- src/main/java/mju/scholarship/health/HealthCheck.java
+++ src/main/java/mju/scholarship/health/HealthCheck.java
@@ -1,7 +1,10 @@
 package mju.scholarship.health;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 public class HealthCheck {
 
     @GetMapping("/")
